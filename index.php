6c7406f1faab6426a88c51e45d923dacec49ffa0
nkhandwe <nkhandwe@gmail.com>
2022-05-07 16:33:20 +0530

    all done

--- index.php
+++ index.php
@@ -77,62 +77,53 @@
                       </li>
                      <!--  <li class="rd-nav-item"><a class="rd-nav-link" href="#">Our Services</a>-->
                       <li class="rd-nav-item"><a class="rd-nav-link" href="#">Our Services</a>
-                        <!-- RD Navbar Dropdown-->
+                       <!-- RD Navbar Dropdown-->
                         <ul class="rd-menu rd-navbar-dropdown">
 
 
                           <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="realestate.php">Real estate →</a>
 
                             <ul class="rd-menu rd-navbar-dropdown">
-                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="realestate.php">Residential, commercial or business transaction → <br>
-                                Sale<br>
-                                Purchase<br>
-                                Refinance
-                              </a>
-
-                    
-
-                                </li>
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="residentialcommercialorbusinesstransaction.php">Residential, commercial or business transaction → </a></li>
+                               <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="sale.php">Sale →</a></li>
+                               <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="purchase.php">Purchase →</a></li>
+                               <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="refinance.php">Refinance →</a></li>
+                              
                               </ul>
 
                           </li>
-
                           <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="immigration.php">Immigration →</a>
+
                             <ul class="rd-menu rd-navbar-dropdown">
-                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="immigration.php">Spousal application <br>
-                                PR application <br>
-                                PRRA Application <br>
-                                Refusal appeals</a>
-                              </li>
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="spousalapplication.php">Spousal application → </a></li>
+                               <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="prapplication.php">PR application →</a></li>
+                               <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="prraapplication.php">PRRA Application →</a></li>
+                               <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="refusalappeals.php">Refusal appeals →</a></li>
+                              </ul>
+                          </li>
+                           <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="familylaw.php">Family law →</a>
+                            <ul class="rd-menu rd-navbar-dropdown">
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="sepration.php">Seperation → </a></li>
+                               <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="divorcematters.php">Divorce matters →</a></li>
+                               <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="propertysettlement.php">Property settlement →</a></li>
                               </ul>
                           </li>
 
-                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="familylaw.php">Family law →</a>
+                            <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="personalinjury.php">Personal injury →</a>
                             <ul class="rd-menu rd-navbar-dropdown">
-                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="familylaw.php">Seperation <br>
-                                Divorce matters<br>
-                                Property settlement </a>
-                              </li>
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="motorvehicalaccident.php">Motor vehicle accident → </a></li>
                               </ul>
                           </li>
-
-                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="personalinjury.php">Personal injury →</a>
+                         
+                            <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="willsandestate.php">Wills and estate →</a>
                             <ul class="rd-menu rd-navbar-dropdown">
-                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="personalinjury.php">Motor vehicle accident </a>
-                              </li>
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="wills.php">Wills → </a></li>
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="powerofattorney.php">Power of attorney → </a></li>
                               </ul>
                           </li>
-
-                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="willsandestate.php">Wills and estate →</a>
-                            <ul class="rd-menu rd-navbar-dropdown">
-                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="willsandestate.php">Wills <br>
-                                Power of attorney </a>
-                              </li>
-                              </ul>
+                         <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="notaryandattestation.php">Notary and attestation →</a>
+                            
                           </li>
-
-                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="notaryandattestation.php">Notary and attestation </a></li>
-                          
                         </ul>
                       </li>
                      <!--  <li class="rd-nav-item"><a class="rd-nav-link" href="practice-areas.php">Practice Areas</a>
