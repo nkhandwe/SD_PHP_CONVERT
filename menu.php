08e10ca0042b4ea04a210a30c03bf8f62096de50
nkhandwe <nkhandwe@gmail.com>
2022-05-07 08:39:53 +0530

    about us page done

---
+++ menu.php
@@ -0,0 +1,121 @@
+<div class="rd-navbar-main-outer">
+              <div class="rd-navbar-main">
+                <!-- RD Navbar Panel-->
+                <div class="rd-navbar-panel">
+                  <!-- RD Navbar Toggle-->
+                  <button class="rd-navbar-toggle" data-rd-navbar-toggle=".rd-navbar-nav-wrap"><span></span></button>
+                  <!-- RD Navbar Brand-->
+                  <div class="rd-navbar-brand"><a class="brand" href="index.php"><img class="brand-logo-dark" src="images/logo-default-159x40.png" alt="" width="159" height="40" srcset="images/logo-default-318x80.png 2x"/><img class="brand-logo-light" src="images/logo-inverse-159x40.png" alt="" width="159" height="40" srcset="images/logo-inverse-318x80.png 2x"/></a>
+                  </div>
+                </div>
+                <div class="rd-navbar-main-element">
+                  <div class="rd-navbar-nav-wrap">
+                    <!-- RD Navbar Nav-->
+                    <ul class="rd-navbar-nav">
+                      <li class="rd-nav-item active"><a class="rd-nav-link" href="index.php">Home</a>
+                      </li>
+                     <!--  <li class="rd-nav-item"><a class="rd-nav-link" href="#">Our Services</a>-->
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="#">Our Services</a>
+                        <!-- RD Navbar Dropdown-->
+                        <ul class="rd-menu rd-navbar-dropdown">
+
+
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="realestate.php">Real estate →</a>
+
+                            <ul class="rd-menu rd-navbar-dropdown">
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="realestate.php">Residential, commercial or business transaction → <br>
+                                Sale<br>
+                                Purchase<br>
+                                Refinance
+                              </a>
+
+                    
+
+                                </li>
+                              </ul>
+
+                          </li>
+
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="immigration.php">Immigration →</a>
+                            <ul class="rd-menu rd-navbar-dropdown">
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="immigration.php">Spousal application <br>
+                                PR application <br>
+                                PRRA Application <br>
+                                Refusal appeals</a>
+                              </li>
+                              </ul>
+                          </li>
+
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="familylaw.php">Family law →</a>
+                            <ul class="rd-menu rd-navbar-dropdown">
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="familylaw.php">Seperation <br>
+                                Divorce matters<br>
+                                Property settlement </a>
+                              </li>
+                              </ul>
+                          </li>
+
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="personalinjury.php">Personal injury →</a>
+                            <ul class="rd-menu rd-navbar-dropdown">
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="personalinjury.php">Motor vehicle accident </a>
+                              </li>
+                              </ul>
+                          </li>
+
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="willsandestate.php">Wills and estate →</a>
+                            <ul class="rd-menu rd-navbar-dropdown">
+                              <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="willsandestate.php">Wills <br>
+                                Power of attorney </a>
+                              </li>
+                              </ul>
+                          </li>
+
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="notaryandattestation.php">Notary and attestation </a></li>
+                          
+                        </ul>
+                      </li>
+                     <!--  <li class="rd-nav-item"><a class="rd-nav-link" href="practice-areas.php">Practice Areas</a>
+                      </li> -->
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="news.php">Blog</a>
+                        <!-- RD Navbar Dropdown-->
+                        <ul class="rd-menu rd-navbar-dropdown">
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="single-post.php">Single Post</a></li>
+                        </ul>
+                      </li>
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="about.php">About Us</a>
+                      </li>
+                    
+                    <!--   <li class="rd-nav-item"><a class="rd-nav-link" href="#">Pages</a>
+                        RD Navbar Dropdown
+                        <ul class="rd-menu rd-navbar-dropdown">
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="buttons.php">Buttons</a></li>
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="forms.php">Forms</a></li>
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="grid-system.php">Grid System</a></li>
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="progress-bars.php">Progress Bars</a></li>
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="tables.php">Tables</a></li>
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="tabs-and-accordions.php">Tabs and Accordions</a></li>
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="typography.php">Typography</a></li>
+                          <li class="rd-dropdown-item"><a class="rd-dropdown-link" href="privacy-policy.php">Privacy Policy</a></li>
+                        </ul>
+                      </li>-->
+                      <li class="rd-nav-item"><a class="rd-nav-link" href="contacts.php">Contact Us</a>
+                      </li>
+                    </ul>
+                  </div>
+                  <!-- RD Navbar Search-->
+                  <div class="rd-navbar-search">
+                    <button class="rd-navbar-search-toggle rd-navbar-fixed-element-2" data-rd-navbar-toggle=".rd-navbar-search"><span></span></button>
+                    <form class="rd-search" action="search-results.php" data-search-live="rd-search-results-live" method="GET">
+                      <div class="form-wrap">
+                        <label class="form-label" for="rd-navbar-search-form-input">Search</label>
+                        <input class="rd-navbar-search-form-input form-input" id="rd-navbar-search-form-input" type="text" name="s" autocomplete="off">
+                        <div class="rd-search-results-live" id="rd-search-results-live"></div>
+                      </div>
+                      <button class="rd-search-form-submit fa-search" type="submit"></button>
+                    </form>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </nav>
+        </div>
